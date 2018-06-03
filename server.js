5c349433fd71d20ef2880a1102163b387f058e9e
nwesthoff <nilsepils94@hotmail.com>
2018-06-03 14:42:06 +0200

    make server return sonarr calendar on /calendar

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 let express = require('express')
 let request = require('request')
 let querystring = require('querystring')
+let fetch = require('node-fetch')
 
 let app = express()
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
+app.set('json spaces', 2)
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response;
+}
+
+let redirect_uri =
+  process.env.REDIRECT_URI ||
   'http://localhost:8888/callback'
 
 app.get('/login', function(req, res) {
@@ -13,7 +23,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email user-read-playback-state user-read-recently-played',
       redirect_uri
     }))
 })
@@ -39,6 +49,43 @@ app.get('/callback', function(req, res) {
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
     res.redirect(uri + '?access_token=' + access_token)
   })
+})
+
+
+app.get('/calendar', function(req, res) {
+  let calendar = [],
+      apikey = process.env.SONARR_API_KEY
+
+  function getSonarrCalendar(apikey) {
+    fetch('http://sonarr.gladosplex.nl/api/calendar?apikey=' + apikey,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+    .then(response => {
+      handleErrors(response)
+      return response.clone()
+    })
+    .then(responseClone => responseClone.json())
+    .then(sonarrCalendarPromise => {
+        console.log("Calling up Sonarr to check schedule")
+
+        sonarrCalendarPromise.forEach(item => {
+          calendar.push(item)
+        })
+
+        res.contentType('application/json')
+        res.json(calendar)
+
+        console.log("Got a schedule from Sonarr, we can probably figure this out")
+      }
+    )
+    .catch(error => console.log(error))
+  }
+
+  getSonarrCalendar(apikey)
 })
 
 let port = process.env.PORT || 8888
